d3ee5a85cc62824461a3987dd087d1eac428412b
Jeroen Zwaal <jeroen@zwaaltjes.nl>
2025-06-05 15:54:21 +0200

    Database Setup

---
+++ WasteWatchAIBackend/Migrations/20250605134951_InitialCreate.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WasteWatchAIBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PredictionResults",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Latitude = table.Column<float>(type: "real", nullable: false),
+                    Longitude = table.Column<float>(type: "real", nullable: false),
+                    Prediction = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FeatureWeather = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FeatureTemp = table.Column<float>(type: "real", nullable: false),
+                    FeatureLocationType = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PredictionResults", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TrashItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LitterType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Latitude = table.Column<float>(type: "real", nullable: false),
+                    Longitude = table.Column<float>(type: "real", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrashItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WeatherData",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Latitude = table.Column<float>(type: "real", nullable: false),
+                    Longitude = table.Column<float>(type: "real", nullable: false),
+                    Temperature = table.Column<float>(type: "real", nullable: false),
+                    WeatherDescription = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherData", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PredictionResults");
+
+            migrationBuilder.DropTable(
+                name: "TrashItems");
+
+            migrationBuilder.DropTable(
+                name: "WeatherData");
+        }
+    }
+}
---
+++ WasteWatchAIBackend/Models/WeatherData.cs
@@ -0,0 +1,12 @@
+﻿namespace WasteWatchAIBackend.Models
+{
+    public class WeatherData
+    {
+        public Guid Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public float Temperature { get; set; }
+        public string WeatherDescription { get; set; }
+    }
+}
