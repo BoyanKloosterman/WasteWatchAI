30726ddae39d12d708dca1e04f7517cfa7aeb584
Pluk Zwaal <40401658+PlukZwaal@users.noreply.github.com>
2025-06-05 20:48:07 +0200

    Created get trashitem api call

---
+++ WasteWatchAIBackend/Controllers/TrashItemsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WasteWatchAIBackend.Data;
+using WasteWatchAIBackend.Models;
+
+namespace WasteWatchAIBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrashItemsController : ControllerBase
+    {
+        private readonly WasteWatchDbContext _context;
+
+        public TrashItemsController(WasteWatchDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TrashItem>>> GetTrashItems()
+        {
+            return await _context.TrashItems.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TrashItem>> GetTrashItem(Guid id)
+        {
+            var item = await _context.TrashItems.FindAsync(id);
+            if (item == null) return NotFound();
+            return item;
+        }
+    }
+}
