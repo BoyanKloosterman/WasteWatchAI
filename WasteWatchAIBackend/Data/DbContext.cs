9824b3db5579f9ce0f590368e99c621fe3aebb51
Boyan Kloosterman <kloostermanboyan@gmail.com>
2025-06-06 16:24:40 +0200

    added dummytrashitems

--- WasteWatchAIBackend/Data/DbContext.cs
+++ WasteWatchAIBackend/Data/DbContext.cs
@@ -11,6 +11,8 @@ namespace WasteWatchAIBackend.Data
         public DbSet<WeatherData> WeatherData { get; set; }
         public DbSet<PredictionResult> PredictionResults { get; set; }
         public DbSet<TrashItem> TrashItems { get; set; }
-        
+
+        public DbSet<DummyTrashItem> DummyTrashItems { get; set; }
+
     }
 }
