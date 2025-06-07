60f8440e5eccab23ab02c664f0795f14e6d2078b
Boyan Kloosterman <kloostermanboyan@gmail.com>
2025-06-07 19:48:54 +0200

    moved models to /models folder

---
+++ WasteWatchAIFrontend/Models/CorrelationScatter.cs
@@ -0,0 +1,12 @@
+﻿namespace WasteWatchAIFrontend.Models
+{
+
+    public class CorrelationScatter
+    {
+        [JsonPropertyName("temperature")]
+        public List<double> Temperature { get; set; } = new();
+
+        [JsonPropertyName("trash_count")]
+        public List<int> TrashCount { get; set; } = new();
+    }
+}
---
+++ WasteWatchAIFrontend/Models/WeatherDistribution.cs
@@ -0,0 +1,11 @@
+﻿namespace WasteWatchAIFrontend.Models
+{
+    public class WeatherDistribution
+    {
+        [JsonPropertyName("labels")]
+        public List<string> Labels { get; set; } = new();
+
+        [JsonPropertyName("values")]
+        public List<int> Values { get; set; } = new();
+    }
+}
