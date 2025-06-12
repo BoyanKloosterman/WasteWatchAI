edf22535c986a2db107d357545d354d864e2400f
Jeroen Zwaal <jeroen@zwaaltjes.nl>
2025-06-12 23:49:00 +0200

    changed prediction model

---
+++ WasteWatchAIBackend/Models/CategoryPrediction.cs
@@ -0,0 +1,15 @@
+﻿namespace WasteWatchAIBackend.Models
+{
+    public class CategoryPrediction
+    {
+        public Guid Id { get; set; } = Guid.NewGuid(); // unieke ID
+        public string Category { get; set; }
+        public int PredictedValue { get; set; }
+        public float ConfidenceScore { get; set; }
+        public string ModelUsed { get; set; }
+
+        // Foreign key if using EF Core
+        public Guid PredictionResultId { get; set; }
+        public PredictionResult PredictionResult { get; set; }
+    }
+}
