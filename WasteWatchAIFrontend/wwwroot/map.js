b0a09c638b0573b204bcff0185dbb51eb22e5f2a
Simon-de-Programmeur <simonlaugeman@gmail.com>
2025-06-16 14:37:27 +0200

    Bezig met het toevoegen van het ophalen van de data

--- WasteWatchAIFrontend/wwwroot/map.js
+++ WasteWatchAIFrontend/wwwroot/map.js
@@ -1,12 +1,32 @@
-﻿var map = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+﻿// Global map variables
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
 
-var popup = L.popup()
-    .setLatLng([51.513, -0.09])
-    .setContent("I am a standalone popup.")
-    .openOn(map);
+//window.map = {
+//    initMap: function (elementId, lat, lng, zoom) {
+//        var map = L.map(elementId).setView([lat, lng], zoom);
+
+//        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//            attribution: '&copy; OpenStreetMap contributors'
+//        }).addTo(map);
+
+//        L.marker([lat, lng]).addTo(map)
+//            .bindPopup("Hier ben ik!")
+//            .openPopup();
+//    }
+//};
+
+
+//var map = L.map('map').setView([51.505, -0.09], 13);
+//L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//    maxZoom: 19,
+//    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+//}).addTo(map);
+
+//var marker = L.marker([51.5, -0.09]).addTo(map);
+
+//var popup = L.popup()
+//    .setLatLng([51.513, -0.09])
+//    .setContent("I am a standalone popup.")
+//    .openOn(map);
+
+
