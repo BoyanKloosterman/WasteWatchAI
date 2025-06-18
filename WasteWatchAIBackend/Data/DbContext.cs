e3efd8280e18c24b80abb5da40c54fa2a95145d6
Boyan Kloosterman <kloostermanboyan@gmail.com>
2025-06-18 15:13:30 +0200

    fixed dbcontext

--- WasteWatchAIBackend/Data/DbContext.cs
+++ WasteWatchAIBackend/Data/DbContext.cs
@@ -12,17 +12,13 @@ namespace WasteWatchAIBackend.Data
         }
         //public DbSet<Weather> Weather { get; set; }
         public DbSet<WeatherData> WeatherData { get; set; }
-        public DbSet<PredictionResult> PredictionResults { get; set; }
         public DbSet<CategoryPrediction> CategoryPredictions { get; set; }
         public DbSet<TrashItem> TrashItems { get; set; }
         public DbSet<DummyTrashItem> DummyTrashItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<PredictionResult>()
-                .HasMany(p => p.Predictions)
-                .WithOne(cp => cp.PredictionResult)
-                .HasForeignKey(cp => cp.PredictionResultId);
+
         }
     }
 }
